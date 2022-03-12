23a3c3413328b9126add7d5cad846753a026f761
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2022-03-12 00:40:21 +0000

    WIP: added some test passing code

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -2,6 +2,8 @@ package uk.ac.bris.cs.scotlandyard.model;
 
 import com.google.common.collect.ImmutableList;
 
+import java.util.LinkedList;
+
 import javax.annotation.Nonnull;
 
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
@@ -17,9 +19,25 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			GameSetup setup,
 			Player mrX,
 			ImmutableList<Player> detectives) {
-		// TODO
+		if (mrX == null) throw new NullPointerException();
+		if (detectives == null) throw new NullPointerException();
+		if (detectives.contains(null)) throw new NullPointerException();
+		LinkedList<Player> detectiveCopy = ImmutableList.copyOf(detectives);
+		throw new IllegalArgumentException();
+
 		throw new RuntimeException("Implement me!");
 
 	}
-
+	//FUNCTIONS 
+	 private final class MyGameState implements GameState {
+		@Override public GameSetup getSetup() {  return null; }
+		@Override  public ImmutableSet<Piece> getPlayers() { return null; }
+		@Nonnull Optional<Integer> getDetectiveLocation(Detective detective) { return null; }
+		@Nonnull Optional<TicketBoard> getPlayerTickets(Piece piece) { return null; }
+		@Nonnull ImmutableList<LogEntry> getMrXTravelLog() { return null; }
+		@Nonnull ImmutableSet<Piece> getWinner() { return null; }
+		@Nonnull ImmutableSet<Move> getAvailableMoves() { return null; }
+		@Override public GameState advance(Move move) {  return null;  }
+	}
+	//FUNCTIONS END
 }
