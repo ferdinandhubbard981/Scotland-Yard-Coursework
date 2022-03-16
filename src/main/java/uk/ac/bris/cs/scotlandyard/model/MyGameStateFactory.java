f8eb0c888c87ae73639a39ee0ecd4abc3ef2daca
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2022-03-16 12:14:50 +0000

    Co-authored-by: ferdinandhubbard981 <ferdinandhubbard@gmail.com>

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -1,13 +1,19 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Optional;
 
 import javax.annotation.Nonnull;
 
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
+import uk.ac.bris.cs.scotlandyard.model.Piece.Detective;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Ticket;
 
 /**
  * cw-model
@@ -19,25 +25,100 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			GameSetup setup,
 			Player mrX,
 			ImmutableList<Player> detectives) {
+		//checking for null inputs
 		if (mrX == null) throw new NullPointerException();
 		if (detectives == null) throw new NullPointerException();
 		if (detectives.contains(null)) throw new NullPointerException();
-		LinkedList<Player> detectiveCopy = ImmutableList.copyOf(detectives);
-		throw new IllegalArgumentException();
+		//end 
 
-		throw new RuntimeException("Implement me!");
+		//check detectives have 0 x2 & secret tickets 
+		detectives.forEach((det) -> {
+			if (det.has(Ticket.DOUBLE)) throw new IllegalArgumentException();
+			if (det.has(Ticket.SECRET)) throw new IllegalArgumentException();
+		});
+		//end
+		
+		//check no duplicate detectives (colour)
+		HashMap<String, Boolean> found = new HashMap<>();
+		for (int i = 0; i < detectives.size(); i++) {
+			String colour = detectives.get(i).piece().webColour();
+			if (found.containsKey(colour)) throw new IllegalArgumentException();
+			found.put(colour, true);
+		}
+		//end
+		
+		//check empty graph & empty moves
+		if(setup.graph.nodes().size() == 0) throw new IllegalArgumentException();
+		if(setup.moves.size() == 0) throw new IllegalArgumentException();
+		//end
 
+		//check detective locations match supplied
+		for(Player detective : detectives){
+			//if (detective.location() != setup.graph)
+		}
+		//end
+
+
+
+
+
+
+
+		
+		return new GameState() {
+
+			@Override
+			public GameSetup getSetup() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public ImmutableSet<Piece> getPlayers() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public Optional<Integer> getDetectiveLocation(Detective detective) {
+				// TODO Auto-generated method stub
+				
+				return null;
+			}
+
+			@Override
+			public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public ImmutableList<LogEntry> getMrXTravelLog() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public ImmutableSet<Piece> getWinner() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public ImmutableSet<Move> getAvailableMoves() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public GameState advance(Move move) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+		};
+
+		//throw new RuntimeException("Implement me!");
+		
 	}
-	//FUNCTIONS 
-	 private final class MyGameState implements GameState {
-		@Override public GameSetup getSetup() {  return null; }
-		@Override  public ImmutableSet<Piece> getPlayers() { return null; }
-		@Nonnull Optional<Integer> getDetectiveLocation(Detective detective) { return null; }
-		@Nonnull Optional<TicketBoard> getPlayerTickets(Piece piece) { return null; }
-		@Nonnull ImmutableList<LogEntry> getMrXTravelLog() { return null; }
-		@Nonnull ImmutableSet<Piece> getWinner() { return null; }
-		@Nonnull ImmutableSet<Move> getAvailableMoves() { return null; }
-		@Override public GameState advance(Move move) {  return null;  }
-	}
-	//FUNCTIONS END
 }
