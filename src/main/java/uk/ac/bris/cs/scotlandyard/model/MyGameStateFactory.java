c04b1b9808918d1da88e53bfd2e3c1e9e8c0e29d
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2022-03-18 17:39:04 +0000

    added initial getSingleMoves impl.

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -7,8 +7,10 @@ import com.google.common.collect.ImmutableSet;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.List;
@@ -118,9 +120,25 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			//update winners based on possible moves list
 			}
 			
+			//implement initial moves getter here
+			
 		}
 
+		private Set<Move> getSingleMoves(
+			GameSetup setup,
+			List<Player> detectives, 
+			Player player, 
+			int source){
+				Set<Move> playerMoves = new HashSet<>();
+				Optional<TicketBoard> tickets = this.getPlayerTickets(player.piece());
+				if(tickets.isEmpty()) throw new IllegalArgumentException();
+				TicketBoard realTickets = tickets.get();
 
+				setup.graph.incidentEdges(source).forEach(pair -> {
+					setup.graph.edgeValue(pair).get(); //returns transport options for edge
+				});
+				return playerMoves;
+		}
 
 		@Override
 		public GameSetup getSetup() {
